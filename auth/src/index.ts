1e9cb197f6dae34a54bb8fb3795d36f89ce1b91c
novaari2 <novaari20@gmail.com>
2025-02-05 21:12:54 +0700

    auth service with unit test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,28 +1,24 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/signin';
-import { signOutRouter } from './routes/signout';
-import { signUpRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import mongoose from 'mongoose';
 
-const app = express();
-app.use(json());
+import { app } from './app';
 
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.all('*', async (req, res) => {
-    throw new NotFoundError();
-});
-
-app.use(errorHandler);
+const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
 
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
-})
+    try{
+
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        console.log('connected to mongo')
+    }catch(err){
+        console.error(err)
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!!!!!!');
+    });
+};
+
+start();
