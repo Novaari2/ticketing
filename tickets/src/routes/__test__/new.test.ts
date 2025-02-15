2e2a389f9cc0acd60435cac45b4f98753330b182
novaari2 <novaari20@gmail.com>
2025-02-15 17:06:40 +0700

    declare test

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+it('has a route handler listening to /api/tickeets for post requests', () => {
+
+});
+
+it('can only be accessed if the user is signed in', () => {
+
+});
+
+it('returns an error if an invalid title is provided', () => {
+
+});
+
+it('returns an error if an invalid price is provided', () => {
+
+});
+
+it('creates a ticket with valid inputs', () => {
+
+});
