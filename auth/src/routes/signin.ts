13912fde62619b648dcb90bbb961b18f6b6995ba
novaari2 <novaari20@gmail.com>
2025-02-15 14:31:15 +0700

    back errord and middleware packages

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -4,8 +4,8 @@ import jwt from 'jsonwebtoken';
 
 import { Password } from '../utils/password';
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request'
+import { BadRequestError } from '../errors/bad-request-error'
 
 const router = express.Router();
 
